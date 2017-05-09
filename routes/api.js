fecb41736f68bfe5d554b91ad492e0e349a71633
Nazarii Beseniuk <prorok97ok@gmail.com>
2017-05-09 15:48:28 +0300

    Add api route for add managers to line,users search by value

--- routes/api.js
+++ routes/api.js
@@ -112,7 +112,7 @@ router.post('/lines/:page?', function (req, res, next) {
 // get line managers
 router.get('/line/managers/:lineid?', function (req, res, next) {
     var lineid = req.params.lineid;
-    console.log(lineid);
+
     if (lineid > 0) {
         Promise.props({
             managers: Line.findOne({id: lineid}).select('managers').lean()
@@ -127,7 +127,6 @@ router.get('/line/managers/:lineid?', function (req, res, next) {
             })
                 .select(['id', 'username', 'profile_picture_circle', 'permission_level', 'realname'])
                 .exec(function (err, users) {
-                    console.warn(users);
                     var data = {
                         data: users
                     };
@@ -144,6 +143,7 @@ router.get('/line/managers/:lineid?', function (req, res, next) {
     }
 });
 
+//get user information to search
 router.get('/users/usersname', function (req, res, next) {
 	Promise.props({
 		users: User.find({}).execAsync()
@@ -193,6 +193,19 @@ router.get('/user/lines/:id?', function (req, res, next) {
 		})
 		.catch(function (err) {
 			next(err)
+		});
+});
+
+router.post('/line/manager/add', function (req, res, next) {
+
+	let body = req.body;
+	Promise.props({
+		line: Line.update( {id: body.lineId}, { $push: { "managers" : { user_id: body.id } } } ).execAsync()
+	}).then(function (results) {
+		res.send(200);
+	})
+		.catch(function (err) {
+			next(err);
 		});
 });
 
