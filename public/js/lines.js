a91746fbc651a838fe2f6a4411186de3502e4592
Nazarii Beseniuk <prorok97ok@gmail.com>
2017-05-10 16:17:53 +0300

    remove unusable ajax

--- public/js/lines.js
+++ public/js/lines.js
@@ -3,34 +3,8 @@ var global = {};
 var $gallery = $('#lines-gallery');
 
 $(document).ready(function () {
-    var CountryToCities = [];
+
     var form = $('#form_add_user');
-
-    $.getJSON('/data/countriesToCities.json', function (data) {
-        var selectCountry = [];
-        $.each(data, function (key, val) {
-            var selectItem = [];
-            selectCountry.push('<option value="' + key + '">' + key + '</option>');
-            $.each(val, function (_key, _val) {
-                selectItem.push('<option value="' + _val + '">' + _val + '</option>');
-            });
-            CountryToCities[key] = selectItem;
-        });
-
-        $('#form-line-country-select').html(selectCountry.join(""));
-
-    });
-
-    $('#form-line-country-select').on({
-        change: function () {
-            appendSelectItem($(this).val());
-        }
-    });
-
-    function appendSelectItem(countryName) {
-        $('#form-line-city-select').html(CountryToCities[countryName].join(""));
-    }
-
 
     buildLines();
 
