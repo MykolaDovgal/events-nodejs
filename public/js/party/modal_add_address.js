88d9e06d1a70cafd4847ffa2ed90f359e860d8ed
Tegos <mihavko_ivan@mail.ru>
2017-05-16 15:27:46 +0300

    Added geocomplete for party

---
+++ public/js/party/modal_add_address.js
@@ -0,0 +1,38 @@
+/**
+ * Created by tegos on 16.05.2017.
+ */
+
+$(document).ready(function () {
+
+
+	$('#modal_add_party').on('shown.bs.modal', function () {
+		initGeoAddParty();
+	})
+
+});
+
+
+window.initGeoAddParty = function () {
+
+
+	var geocomplete_line_add = $('#geocomplete_party_add').geocomplete({
+		details: '.geo-data',
+		//types: ['(cities)']
+
+	}).on('geocode:result', function (e, result) {
+		var geo_data = $('.geo-data');
+		var data = {
+			lat: $('#lat').val(),
+			lng: $('#lng').val(),
+			locality: $('#locality').val(),
+			country: $('#country').val(),
+			country_short: $('#country_short').val(),
+			route: $('#route').val(),
+			street_number: $('#street_number').val(),
+		};
+
+
+		console.log(data);
+
+	});
+};
