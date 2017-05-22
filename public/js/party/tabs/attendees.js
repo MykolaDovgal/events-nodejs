9ab3996a036a12094149d3183c899b9f1b1c751a
Nazarii Beseniuk <prorok97ok@gmail.com>
2017-05-22 16:08:39 +0300

    attendees complete

--- public/js/party/tabs/attendees.js
+++ public/js/party/tabs/attendees.js
@@ -22,38 +22,65 @@ let initAttendeesTable = function () {
 		"columns": [
 			{
 				'data': 'user_picture',
+				render: function (data, type, full, meta) {
+					return `<div class="text-center"><img class="profile-picture" src="${data}"/></div>`;
+				},
 				width: '10%'
 			},
 			{
 				'data': 'userId',
+				render: function (data, type, full, meta) {
+					return `<div class="text-center">${data}</div>`;
+				},
 				width: '10%'
 			},
 			{
 				'data': 'user_name',
+				render: function (data, type, full, meta) {
+					return `<div class="text-center">${data}</div>`;
+				},
 				width: '10%'
 			},
 			{
 				'data': 'attend_mark_time',
+				render: function (data, type, full, meta) {
+					return `<div class="text-center">${data}</div>`;
+				},
 				width: '10%'
 			},
 			{
 				'data': 'ticket_purchase',
+				render: function (data, type, full, meta) {
+					return `<div class="text-center"><input disabled type="checkbox" ${data == true ? 'checked' : ''} ></div>`;
+				},
 				width: '10%'
 			},
 			{
 				'data': 'ticket_checkin',
+				render: function (data, type, full, meta) {
+					return `<div class="text-center"><input disabled type="checkbox" ${data == true ? 'checked' : ''} ></div>`;
+				},
 				width: '10%'
 			},
 			{
 				'data': 'checkin_time',
+				render: function (data, type, full, meta) {
+					return `<div class="text-center">${data}</div>`;
+				},
 				width: '10%'
 			},
 			{
 				'data': 'location_ver',
+				render: function (data, type, full, meta) {
+					return `<div class="text-center"><input disabled type="checkbox" ${data == true ? 'checked' : ''} ></div>`;
+				},
 				width: '10%'
 			},
 			{
 				'data': 'location_ver_time',
+				render: function (data, type, full, meta) {
+					return `<div class="text-center">${data}</div>`;
+				},
 				width: '10%'
 			},
 
--- routes/party/api/attendees.js
+++ routes/party/api/attendees.js
@@ -3,6 +3,9 @@ let router = express.Router();
 let Promise = require('bluebird');
 let moment = require('moment');
 
+let config = require('config');
+let default_image_user = config.get('images:default_image_user');
+
 let Party = require('models/Party');
 let User = require('models/user');
 
@@ -13,36 +16,37 @@ router.get('/party/:id/attendees', function (req, res, next) {
 	})
 		.then(function (results) {
 			let data = [];
+			let fakeUsersId = [];
 
-			console.warn(results.party.attendees);
+			// console.warn(results.party.attendees);
+
+
 
 			results.party.attendees.forEach(function (attendee, index) {
-				let user_name = '';
+				let user_name = 'test user';
+				let user_pic = '';
 
-				User.findOne({ id: attendee.userId }).select('username')
+				User.findOne({ id: fakeUsersId.pop() }).select('username')
 					.then(function(res){
-						user_name = res;
+						user_name = res.username;
+						user_pic = res.profile_picture_circle;
 					})
 					.catch(function (err){
-						user_name = 'test info';
 						next(err);
 					});
 
 
 				data.push({
-					party_id: party.id,
-					line_name_eng: "test data",
-					country_name_eng: party.location.country,
-					city_name_eng: party.location.city,
-					event_name_eng: "test data",
-					date: moment(party.date).format('DD/MM/YYYY'),
-					open_time: moment( party.open_time).format('HH:mm'),
-					attendees_count: 0,
-					video_stream_avb: false,
-					tkts_avbl_here: false
+					user_picture: default_image_user,
+					userId: attendee.userId,
+					user_name: user_name,
+					attend_mark_time:  attendee.attend_mark_time ? moment(attendee.attend_mark_time).format('DD/MM/YYYY HH:mm') : '',
+					ticket_purchase: attendee.ticket_purchase,
+					ticket_checkin:  attendee.ticket_checkin,
+					checkin_time :attendee.checkin_time ? moment(attendee.checkin_time).format('DD/MM/YYYY HH:mm') : '',
+					location_ver: attendee.location_ver,
+					location_ver_time: attendee.location_ver_time ? moment(attendee.location_ver_time).format('DD/MM/YYYY HH:mm') : ''
 				});
-
-
 			});
 			let temp = {data: data};
 			res.json(temp);
