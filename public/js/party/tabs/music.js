89539fdac5d0edbe918bc58d4f910785e905e43d
Nazarii Beseniuk <prorok97ok@gmail.com>
2017-05-24 16:58:03 +0300

    generate editable tab

--- public/js/party/tabs/music.js
+++ public/js/party/tabs/music.js
@@ -1,4 +1,4 @@
-let stageCount = 2;
+let stageCount = 0;
 
 $(document).ready(function () {
 
@@ -8,11 +8,15 @@ $(document).ready(function () {
 		generateStageTab();
 	});
 
+	$('body').on('click','.edit_btn_flag',function () {
+		let stageName = $(this).parent('.panel-heading').find('a.editable');
+		stageName.editable('toggleDisabled');
+		let isCollapse = stageName.attr('data-toggle') == 'collapse' ? '' : 'collapse';
+		stageName.attr('data-toggle', isCollapse);
+	});
 });
 
 let generateStageTab = function () {
-
-	let stageTemplate;
 
 	$.ajax({
 		url: '/api//party/music/stage/add',
@@ -20,94 +24,58 @@ let generateStageTab = function () {
 		data: {partyId: party.id},
 		success: function (_id) {
 
-			stageTemplate = $(`
-					<div class="panel panel-default">
-			
-			            <div class="panel-heading">
-			                <a id="party_stage_${stageCount}_name" class="editable editable-click editable-disabled" href="#stage_${stageCount}_body"
-			                   style="margin:10px;display: inline-block" data-type="text" data-pk="${_id}"
-			                   data-parent="#music_accordion_container">LOOOOOOOOOOOOOOOOOOOOOL</a>
-			                <button id="enable_stage_${stageCount}_edit" class="edit_btn_flag" type="button">
-			                    <i class="fa fa-pencil"></i>
-			                </button>
-			            </div>
-			
-			            <div id="stage_${stageCount}_body" class="panel-collapse in">
-			                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias, harum impedit obcaecati perspiciatis quas quisquam voluptatum! Alias, aliquid asperiores commodi delectus, in ipsum, magnam molestiae nesciunt praesentium ullam ut?
-			                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias, harum impedit obcaecati perspiciatis quas quisquam voluptatum! Alias, aliquid asperiores commodi delectus, in ipsum, magnam molestiae nesciunt praesentium ullam ut?
-			                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias, harum impedit obcaecati perspiciatis quas quisquam voluptatum! Alias, aliquid asperiores commodi delectus, in ipsum, magnam molestiae nesciunt praesentium ullam ut?
-			                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias, harum impedit obcaecati perspiciatis quas quisquam voluptatum! Alias, aliquid asperiores commodi delectus, in ipsum, magnam molestiae nesciunt praesentium ullam ut?
-			                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias, harum impedit obcaecati perspiciatis quas quisquam voluptatum! Alias, aliquid asperiores commodi delectus, in ipsum, magnam molestiae nesciunt praesentium ullam ut?
-			                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias, harum impedit obcaecati perspiciatis quas quisquam voluptatum! Alias, aliquid asperiores commodi delectus, in ipsum, magnam molestiae nesciunt praesentium ullam ut?
-			
-			            </div>
-			
-			        </div>
-			`);
-
+			let stageTemplate = getTabTemplate(stageCount,_id);
 			$('#music_accordion_container').append(stageTemplate);
-
-			$('#party_stage_' + stageCount+ '_name').editable({
-				type: 'text',
-				name: 'stageTab',
-				title: 'Enter title',
-				// success: function(data) {
-				// 	$('#english_title').text(data);
-				// }
-			});
-
-			$('body').on('click','.edit_btn_flag',function () {
-				let stageName = $(this).find('a.editable');
-				stageName.editable('toggleDisabled');
-				let isCollapse = stageName.attr('data-toggle') == 'collapse' ? '' : 'collapse';
-				stageName.attr('data-toggle', isCollapse);
-			});
+			setEditable(stageCount);
 			stageCount+=1;
-		},
-		error: function (jqXHR, textStatus, err) {
+
 		}
-	}).then(function () {
 	});
 
 
 };
 
+let setEditable = function (counter) {
+
+	$('#party_stage_' + counter+ '_name').editable({
+		type: 'text',
+		name: 'stageTab',
+		title: 'Enter title',
+	});
+};
+
+let getTabTemplate = function (counter,_id) {
+
+	return $(`
+					<div class="panel panel-default">			
+			            <div class="panel-heading">
+			                <a id="party_stage_${counter}_name" class="editable editable-click editable-disabled" href="#stage_${counter}_body"
+			                   style="margin:10px;display: inline-block" data-type="text" data-pk="${_id}"
+			                   data-parent="#music_accordion_container">LOOOOOOOOOOOOOOOOOOOOOL</a>
+			                <button id="enable_stage_${counter}_edit" class="edit_btn_flag" type="button">
+			                    <i class="fa fa-pencil"></i>
+			                </button>
+			            </div>
+			            <div id="stage_${counter}_body" class="panel-collapse in">
+			                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias, harum impedit obcaecati perspiciatis quas quisquam voluptatum! Alias, aliquid asperiores commodi delectus, in ipsum, magnam molestiae nesciunt praesentium ullam ut?
+			                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias, harum impedit obcaecati perspiciatis quas quisquam voluptatum! Alias, aliquid asperiores commodi delectus, in ipsum, magnam molestiae nesciunt praesentium ullam ut?
+			                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias, harum impedit obcaecati perspiciatis quas quisquam voluptatum! Alias, aliquid asperiores commodi delectus, in ipsum, magnam molestiae nesciunt praesentium ullam ut?
+			                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias, harum impedit obcaecati perspiciatis quas quisquam voluptatum! Alias, aliquid asperiores commodi delectus, in ipsum, magnam molestiae nesciunt praesentium ullam ut?
+			                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias, harum impedit obcaecati perspiciatis quas quisquam voluptatum! Alias, aliquid asperiores commodi delectus, in ipsum, magnam molestiae nesciunt praesentium ullam ut?
+			                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias, harum impedit obcaecati perspiciatis quas quisquam voluptatum! Alias, aliquid asperiores commodi delectus, in ipsum, magnam molestiae nesciunt praesentium ullam ut?
+			
+			            </div>			
+			        </div>
+			`);
+};
+
 let FormEditable = function () {
 
-	let initEditables = function () {
-
-		//global settings
-		$.fn.editable.defaults.inputclass = 'form-control';
-		$.fn.editable.defaults.url = '/api/party/music/update';
-		$.fn.editable.defaults.mode = 'inline';
-
-
-		$('#party_stage_1_name').editable({
-			type: 'text',
-			name: 'stageTab',
-			title: 'Enter title',
-			// success: function(data) {
-			// 	$('#english_title').text(data);
-			// }
-		});
-
-	};
-
 	return {
-		//main function to initiate the module
 		init: function () {
-			// init editable elements
-
-			initEditables();
-
-			// init editable toggler
-			$('#enable_stage_1_edit').click(function () {
-				let stageName = $('#party_stage_1_name');
-				stageName.editable('toggleDisabled');
-				let isCollapse = stageName.attr('data-toggle') == 'collapse' ? '' : 'collapse';
-				stageName.attr('data-toggle', isCollapse);
-			});
-
+			$.fn.editable.defaults.inputclass = 'form-control';
+			$.fn.editable.defaults.url = '/api/party/music/update';
+			$.fn.editable.defaults.mode = 'inline';
 		}
 	};
 }();
