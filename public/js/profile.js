f6e9eb56f208aef9acabbf5a9c68b009257ab221
Ivan <bananachevalier@gmail.com>
2017-05-25 17:48:02 +0300

    Small fix for facebook input field.

--- public/js/profile.js
+++ public/js/profile.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
 
 	var lines;
 
-	toastr.options.showMethod = 'slideDown'; 
+	toastr.options.showMethod = 'slideDown';
 	FormEditable.init();
 
 	// croppie
@@ -33,7 +33,7 @@ $(document).ready(function () {
 			reader.onload = function (e) {
 				$uploadCrop.croppie('bind', {
 					url: e.target.result
-				}).then(function(){
+				}).then(function () {
 					$('#upload-demo').addClass('ready');
 				});
 			};
@@ -71,29 +71,29 @@ $(document).ready(function () {
 	// croppie
 
 	$('#button-change-picture').on('click', function () {
-		$uploadCrop.croppie('result', 'base64').then(function(base64) {
+		$uploadCrop.croppie('result', 'base64').then(function (base64) {
 			$("#userpic").attr("src", base64);
 			$('#form_update_user').submit();
 		});
 	});
 
-	$('#button-open-crop').on('click', function() {
+	$('#button-open-crop').on('click', function () {
 		$('#crop-picture-modal').modal('show');
 		$toCrop.croppie('bind', {
 			url: $('#image-to-crop').attr('src')
 		});
 	});
 
-	$('#button-open-change').on('click', function() {
+	$('#button-open-change').on('click', function () {
 		$('#change-picture-modal').modal('show');
 	});
 
-	$('#button-crop-picture').on('click', function() {
-		$toCrop.croppie('result', 'base64').then(function(base64) {
+	$('#button-crop-picture').on('click', function () {
+		$toCrop.croppie('result', 'base64').then(function (base64) {
 			$('#userpic').attr("src", base64);
-			
+
 			var formData = new FormData($('#form_update_user')[0]);
-			
+
 			$toCrop.croppie('result', {
 				type: 'blob',
 				size: 'viewport',
@@ -101,38 +101,6 @@ $(document).ready(function () {
 			}).then(function (blob) {
 				console.log(blob);
 				formData.append('userpic', blob, 'userpic.png');
-					$.ajax({
-						url: '/user/update/' + user.id,
-						type: 'POST',
-						cache: false,
-						contentType: false,
-						processData: false,
-						data: formData,
-						success: function (data) {
-							$('#crop-picture-modal').modal('hide');
-							toastr.success('Saved!');
-						},
-						error: function (jqXHR, textStatus, err) {
-							toastr.error('Server error!');
-						}
-					});
-			});
-		})
-	});
-
-	$('#form_update_user').submit(function(e) {
-		e.preventDefault();
-
-		var formData = new FormData(this);
-		formData.append('profile-image', $('#form-profile-pic')[0].files[0]);
-		$('#image-to-crop').attr('src', $('#upload-demo .cr-image').attr('src'));
- 
-		$uploadCrop.croppie('result', {
-			type: 'blob',
-			size: 'viewport',
-			circle: true
-		}).then(function (blob) {
-			formData.append('userpic', blob, 'userpic.png');
 				$.ajax({
 					url: '/user/update/' + user.id,
 					type: 'POST',
@@ -141,17 +109,49 @@ $(document).ready(function () {
 					processData: false,
 					data: formData,
 					success: function (data) {
-						$('#change-picture-modal').modal('hide');
+						$('#crop-picture-modal').modal('hide');
 						toastr.success('Saved!');
 					},
 					error: function (jqXHR, textStatus, err) {
 						toastr.error('Server error!');
 					}
 				});
+			});
+		})
+	});
+
+	$('#form_update_user').submit(function (e) {
+		e.preventDefault();
+
+		var formData = new FormData(this);
+		formData.append('profile-image', $('#form-profile-pic')[0].files[0]);
+		$('#image-to-crop').attr('src', $('#upload-demo .cr-image').attr('src'));
+
+		$uploadCrop.croppie('result', {
+			type: 'blob',
+			size: 'viewport',
+			circle: true
+		}).then(function (blob) {
+			formData.append('userpic', blob, 'userpic.png');
+			$.ajax({
+				url: '/user/update/' + user.id,
+				type: 'POST',
+				cache: false,
+				contentType: false,
+				processData: false,
+				data: formData,
+				success: function (data) {
+					$('#change-picture-modal').modal('hide');
+					toastr.success('Saved!');
+				},
+				error: function (jqXHR, textStatus, err) {
+					toastr.error('Server error!');
+				}
+			});
 		});
 	});
 
-	$('#form-date-of-birth').change(function() {
+	$('#form-date-of-birth').change(function () {
 		var date_of_birth = { name: 'date_of_birth', value: $(this).val(), pk: 1 };
 		console.log(date_of_birth);
 		$.ajax({
@@ -163,14 +163,14 @@ $(document).ready(function () {
 		});
 	});
 
-	$('#active-switch').on('switchChange.bootstrapSwitch', function(event, state) {
+	$('#active-switch').on('switchChange.bootstrapSwitch', function (event, state) {
 		var active = { name: 'active', value: state, pk: 1 };
 		$.ajax({
 			url: '/user/update/' + user.id,
 			type: 'POST',
 			dataType: 'json',
 			contentType: "application/json; charset=utf-8",
-			data: JSON.stringify(active)  
+			data: JSON.stringify(active)
 		});
 	});
 
@@ -197,11 +197,11 @@ $(document).ready(function () {
 				responsive: false,
 				"dom": "<'row' <'col-md-12'>> t <'row'<'col-md-12'>>",
 			});
-			$('#table-lines tbody').on('click', 'tr', function () {
-				window.location.href = "/line/" + lines.row(this).data().id;
-			});
+		$('#table-lines tbody').on('click', 'tr', function () {
+			window.location.href = "/line/" + lines.row(this).data().id;
+		});
 	});
- 
+
 });
 
 
@@ -209,7 +209,7 @@ function showDeleteConfirmation() {
 	bootbox.confirm({
 		size: "small",
 		message: "You are about to delete " + user.firstname + "'s profile. Are you sure?",
-		callback: function(result) {
+		callback: function (result) {
 			if (result) {
 				$.ajax({
 					url: '/user/delete/',
@@ -228,8 +228,8 @@ function showDeleteConfirmation() {
 }
 
 //inline edit
-let FormEditable = function() {
-	let initEditables = function() {
+let FormEditable = function () {
+	let initEditables = function () {
 
 
 		//global settings
@@ -246,31 +246,31 @@ let FormEditable = function() {
 			name: 'username',
 			title: 'Enter User Name'
 		});
-		$('#firstname').editable({      
+		$('#firstname').editable({
 			type: 'text',
 			pk: 1,
 			name: 'firstname',
 			title: 'Enter First Name'
 		});
-		$('#lastname').editable({               
+		$('#lastname').editable({
 			type: 'text',
 			pk: 1,
 			name: 'lastname',
 			title: 'Enter Last Name'
 		});
-		$('#facebook').editable({           
+		$('#facebook_profile').editable({
 			type: 'text',
 			pk: 1,
-			name: 'facebook',
+			name: 'facebook_profile',
 			title: 'Enter Facebook Profile'
 		});
-		$('#email').editable({              
+		$('#email').editable({
 			type: 'text',
 			pk: 1,
 			name: 'email',
 			title: 'Enter Email'
 		});
-		$('#phone').editable({              
+		$('#phone').editable({
 			type: 'text',
 			pk: 1,
 			name: 'phone',
@@ -285,23 +285,23 @@ let FormEditable = function() {
 	};
 	return {
 		//main function to initiate the module
-		init: function() {
+		init: function () {
 			// init editable elements
 
 
 			initEditables();
 
 			// init editable toggler
-			$('#enable').click(function() {
+			$('#enable').click(function () {
 				$('#user .editable').editable('toggleDisabled');
 			});
 
 			// handle editable elements on hidden event fired
-			$('#user .editable').on('hidden', function(e, reason) {
+			$('#user .editable').on('hidden', function (e, reason) {
 				if (reason === 'save' || reason === 'nochange') {
 					var $next = $(this).closest('tr').next().find('.editable');
 					if ($('#autoopen').is(':checked')) {
-						setTimeout(function() {
+						setTimeout(function () {
 							$next.editable('show');
 						}, 300);
 					} else {
