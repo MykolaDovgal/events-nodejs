af90bb90768814c3f55af90091c04c6fa83fc538
Nazarii Beseniuk <prorok97ok@gmail.com>
2017-06-20 16:26:41 +0300

    added: x-editable permission level in managers with inheritance(Event)

--- routes/user/api/activity.js
+++ routes/user/api/activity.js
@@ -8,26 +8,38 @@ let config = require('config');
 let default_image_user = config.get('images:default_image_user');
 
 let User = require('models/User');
+let Bar = require('models/Bar');
+let Line = require('models/Line');
+let Party = require('models/Party');
+let Event = require('models/Event');
 
 
 router.get('/users', function (req, res, next) {
 	Promise.props({
-		users: User.find({}).execAsync()
+		users: User.find({}).execAsync(),
+		bar: Bar.find({}, 'managers').execAsync(),
+		event: Event.find({}, 'managers').execAsync(),
+		line: Line.find({}, 'managers').execAsync()
 	})
 		.then(function (results) {
 			let lastActivities = [];
+			let users = [];
+			let userIdArray = [];
 
 			results.users.map(function (user) {
 				let lastActivity = user.getActivity()[user.getActivity().length - 1] ? user.getActivity()[user.getActivity().length - 1].login_time : '-';
 				lastActivities.push(lastActivity);
 				user.profile_picture_circle = user.image_circle;
+				userIdArray.push(user.id);
 				return user;
 			});
 
-			let users = [];
+			let barManagersCount = getManagerCount(results.bar, userIdArray);
+			let eventManagersCount = getManagerCount(results.event, userIdArray);
+			let lineManagersCount = getManagerCount(results.line, userIdArray, 'user_id');
 
-			//TODO FIX THIS IMMEDIATELY
 			results.users.forEach(function (user, index) {
+
 				users.push({
 					id: user.id,
 					username: user.username,
@@ -42,22 +54,17 @@ router.get('/users', function (req, res, next) {
 					facebook_profile: user.facebook_profile,
 					active: user.active,
 					lastActivity: lastActivities[index],
-					//fake data for table
-					bars: Math.floor(Math.sin(user.id).toString().substr(17)),
-					events: Math.floor(Math.sin(user.id).toString().substr(18)),
-					lines: Math.floor(Math.sin(user.id).toString().substr(17) / 2),
+					bars: barManagersCount['id' + user.id] || 0,
+					events: eventManagersCount['id' + user.id] || 0,
+					lines: lineManagersCount['id' + user.id] || 0
 				});
+
 			});
-
-			let data = {
-				data: users,
-			};
-
-			res.json(data);
+			res.json({data: users});
 		})
 		.catch(function (err) {
 			next(err)
-		});
+		})
 });
 
 router.get('/activity/:id?', function (req, res, next) {
@@ -74,6 +81,17 @@ router.get('/activity/:id?', function (req, res, next) {
 	});
 });
 
+let getManagerCount = function (arr, userIdArr, fieldName = 'userId') {
+	let resArray = [];
+	userIdArr.forEach((userId) => {
+		arr.forEach((managers) => {
+			if (!managers.length && managers.managers.some((manager) => manager[fieldName] === userId)) {
+				resArray['id' + userId] = resArray['id' + userId] ? resArray['id' + userId] += 1 : 1;
+			}
+		});
+	});
+	return resArray;
+};
 
 module.exports = router;
 
